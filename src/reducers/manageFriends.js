398afeae8c98ebe8d29fa90e39be712bdd94aef5
jisung594 <jisung594@gmail.com>
2019-03-09 15:13:04 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
 export function manageFriends(state, action){
+
+  let friendJoe = { name: 'Joe', hometown: 'Boston', id: 101 }
+
+  switch (action.type) {
+    case 'ADD_FRIEND':
+    // let friendJoe = Object.assign({}, state.friends, { name: 'Joe', hometown: 'Boston', id: 101 })
+      return { friends: [...state.friends, friendJoe] }
+    default:
+      return state;
+
+    case 'REMOVE_FRIEND':
+      let toBeRemoved = state.friends.find(friend => {
+        return friend.id === friendJoe.id
+      })
+      let newArr = state.friends.filter(friend => {
+        return friend !== toBeRemoved
+      })
+      return { friends: newArr }
+    // default:
+    //   return state;
+  }
 }
